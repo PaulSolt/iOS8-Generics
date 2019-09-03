db461c745b0464c7a5af71b6ce086c09def6bb0c
Paul Solt <PaulSolt@gmail.com>
2019-09-03 13:42:30 -0400

    Added Power

---
+++ iOS8-Generics.playground/Pages/Power.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// 2^2 = 2 * 2 = 4
+// 2^3 = 2 * 2 * 2 = 8
+//: [Next](@next)
--- iOS8-Generics.playground/Pages/Queue.xcplaygroundpage/Contents.swift
+++ iOS8-Generics.playground/Pages/Queue.xcplaygroundpage/Contents.swift
@@ -6,19 +6,19 @@ import Foundation
 // Grocery store line
 // struct is a value type, we can't modify data unless we use `mutating`
 // BUG: After creating a new Page, autocomplete will stop working until you switch files
-struct Queue {
-    var elements: [Int]
+struct Queue<Element> {
+    var elements: [Element]
     
     // pop = remove an element
     // push = add an element
     // peek = look at element
     
     // 3 4 5 6
-    mutating func push(_ element: Int) {
+    mutating func push(_ element: Element) {
         elements.append(element)
     }
     
-    mutating func pop() -> Int? {
+    mutating func pop() -> Element? {
         if elements.count > 0 {
             return elements.removeFirst()
         } else {
@@ -26,12 +26,13 @@ struct Queue {
         }
     }
     
-    func peek() -> Int? {
+    func peek() -> Element? {
         return elements.first
     }
 }
 
-var deliQueue = Queue(elements: [54, 55, 56])
+//var deliQueue = Queue(elements: [54, 55, 56]) // Type Inference
+var deliQueue = Queue<Int>(elements: [54, 55, 56])
 
 deliQueue.peek()
 deliQueue.pop()
@@ -44,5 +45,16 @@ deliQueue.pop()
 deliQueue.peek()
 deliQueue.pop()
 
+// URLs
 
+var downloaderQueue = Queue<URL>(elements: [])
+
+downloaderQueue.push(URL(string: "https://apple.com/photos/1")!)
+downloaderQueue.push(URL(string: "https://apple.com/photos/2")!)
+downloaderQueue.push(URL(string: "https://apple.com/photos/3")!)
+downloaderQueue.push(URL(string: "https://apple.com/photos/4")!)
+downloaderQueue.push(URL(string: "https://apple.com/photos/5")!)
+downloaderQueue.push(URL(string: "https://apple.com/photos/6")!)
+
+downloaderQueue.peek()
 //: [Next](@next)
