07159a5e5edebcf6d084570f1df1e722a6d835d0
Paul Solt <PaulSolt@gmail.com>
2019-09-03 13:23:00 -0400

    Added new page

---
+++ iOS8-Generics.playground/Pages/Queue.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// FIFO: First in First out
+// Grocery store line
+struct Queue {
+    private
+    
+    // pop = remove an element
+    // push = add an element
+    // peek = look at element
+    
+}
+
+
+//: [Next](@next)
