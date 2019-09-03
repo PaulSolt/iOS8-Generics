ab82d3d9e2e5181d341ffff8b82b507eb14939d1
Paul Solt <PaulSolt@gmail.com>
2019-09-03 14:03:49 -0400

    Added comments and the code for raising a power

--- iOS8-Generics.playground/Pages/Power.xcplaygroundpage/Contents.swift
+++ iOS8-Generics.playground/Pages/Power.xcplaygroundpage/Contents.swift
@@ -2,6 +2,85 @@
 
 import Foundation
 
+// Power
+// 2^0 = 1
+// 2^1 = 2
 // 2^2 = 2 * 2 = 4
 // 2^3 = 2 * 2 * 2 = 8
+
+//func power(_ value: Int, _ exponent: Int) -> Int {
+//    var result: Int = 1
+//
+//    for _ in 0..<exponent {
+//        result *= value
+//    }
+//    return result
+//}
+
+// Stand alone function (hander to find)
+func power<Value: Numeric>(_ value: Value, _ exponent: Int) -> Value {
+    var result: Value = 1
+    
+    for _ in 0..<exponent {
+        result *= value
+    }
+    return result
+}
+
+power(2, 1)
+power(2, 2)
+power(2.5, 2)
+
+// Use extensions to add behvior to existing types
+
+//extension Int {
+//
+//}
+//extension Double {
+//
+//}
+//
+//extension Int32 {
+//
+//}
+
+let array = [100, 34, 22, 343]
+//array.sort() // in-place sort
+let sortedArray = array.sorted() // return an array of sorted elements (not mutating array)
+
+
+//2.raiseTo(2) // 4
+
+// DRY: Don't Repeat Yourself
+
+extension Numeric { // 2 = Int
+    mutating func raiseTo(_ exponent: Int) {
+        self = self.raisedTo(exponent)  // 2
+    }
+    
+    // Used past tense for returning a result, without modifying a value
+    func raisedTo(_ exponent: Int) -> Self {
+        var result: Self = 1        // Self = type
+        
+        for _ in 0..<exponent {
+            result *= self          // self = currentValue = 2
+        }
+        return result
+    }
+}
+
+var x = 2
+x.raiseTo(3)
+print(x)
+
+let y = 4
+let z = y.raisedTo(2)
+
+// By adding the function to the type using an extension, we make it easier
+// to find using code completition
+
+// Swift Naming Convention
+// Past tense = return a new copy using algorithm
+// Present tense = modify in-place the value using algorithm
+
 //: [Next](@next)
